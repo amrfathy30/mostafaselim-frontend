54d226c96226f67131b99a82c4d5cb301d1bb66c
amira_spokn001 <amira@getspokn.com>
2025-12-30 16:44:39 +0200

    MERGE: books section and the podcast section into main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import HistorySection from '../Components/History/HistorySection';
 import WorksSection from '../Components/works/WorksSection';
-
-
+import BooksSection from '../Components/books-section';
+import PodcastSection from '../Components/podcast-section';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <HistorySection />
       <WorksSection />
-    </div>
+      <PodcastSection />
+      <BooksSection />
+    </>
     )
     
 }
