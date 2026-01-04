ee9d5b56873d95b6f33a77103b11e34773d1ff64
Alaa Sameh <aalaasameh134@gmail.com>
2026-01-04 23:56:31 +0200

    add books section integration

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AuthorHero from '../Components/Hero/AuthorHero';
 import HistorySection from '../Components/History/HistorySection';
 import WorksSection from '../Components/works/WorksSection';
-import BooksSection from '../Components/books-section';
+import BooksSection from "../Components/books/books-section";
 import PodcastSection from '../Components/podcast-section';
 
 export default function Home() {
