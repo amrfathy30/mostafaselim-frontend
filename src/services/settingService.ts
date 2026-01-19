3358ac9910a671d613af0d6fda587e4f8777cc03
Alaa Sameh <aalaasameh134@gmail.com>
2026-01-20 00:33:39 +0200

    setting page

--- src/services/settingService.ts
+++ src/services/settingService.ts
@@ -2,9 +2,10 @@ import axiosInstance from '../api/axiosInstance';
 
 export const getSetting = async () => {
     const response = await axiosInstance.get('/setting');
-    return response.data;
+    return response.data; 
 };
-export const updateSetting = async (data: any) => {
-    const response = await axiosInstance.post('/admin/update/site-setting');
+
+export const updateSetting = async (formData: FormData) => {
+    const response = await axiosInstance.post('/admin/update/site-setting', formData);
     return response.data;
 };
